af81a27501c397f3e7672aa0f3281853f8d03fbc
Soufiane Jarraf <soufiane_jarraf@um5.ac.ma>
2022-02-01 19:55:46 +0100

    updated PriceHistory.java

--- src/main/java/com/example/ensiasea/Entity/PriceHistory.java
+++ src/main/java/com/example/ensiasea/Entity/PriceHistory.java
@@ -1,14 +1,22 @@
 package com.example.ensiasea.Entity;
 
+import javax.persistence.*;
 import java.util.Date;
+
+@Entity
+@Table(name = "priceHistory")
 
 public class PriceHistory {
 
     // foreign keys : "price", "itemId"
-   
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long PriceHistoryID;
+
+    @Column(nullable = false, unique = true, updatable = false)
     private Date date;
+
+    @Column(nullable = false, unique = true, updatable = false)
     private String PreviousOwner;
 
 
